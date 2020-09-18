eb05801050defd4bb1632765bb5eb115a1bb25e1
marshalcn <7664373+marshalcn@user.noreply.gitee.com>
2020-09-18 16:49:30 +0800

    屎一样的命名

---
+++ java/ShitNameFunction.java
@@ -0,0 +1,10 @@
+/*
+* 学生学科评价课程下拉列表加载内容
+* 吐槽一下方法命名，根本就看不懂啥意思，不知道咋写出来的
+*/
+@RequestMapping(value = "/getkcmcxl", method = RequestMethod.POST)
+public Object getListData(@RequestBody String str){
+    List<Coursetb> list = null;
+    // 一大堆操作
+    return list;
+}
